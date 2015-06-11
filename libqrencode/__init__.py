fd877556bbfbf6ddf61581e8fbf05f9899c29ee3
David Wolever <david@wolever.net>
2015-06-11 16:10:43 -0400

    Add some tests, SVG functions

--- libqrencode/__init__.py
+++ libqrencode/__init__.py
@@ -1,7 +1,19 @@
 try:
-    import PIL
+    from PIL import Image
+    from PIL import ImageOps
 except ImportError:
-    PIL = None
+    Image = None
+    ImageOps = None
+
+
+try:
+    try:
+        import lxml.etree as ET
+    except ImportError:
+        import xml.etree.ElementTree as ET
+except ImportError:
+    ET = None
+
 
 from ._ffi import ffi, lib
 
@@ -14,6 +26,11 @@ MODE_STRUCTURE = lib.QR_MODE_STRUCTURE
 MODE_ECI = lib.QR_MODE_ECI
 MODE_FNC1FIRST = lib.QR_MODE_FNC1FIRST
 MODE_FNC1SECOND = lib.QR_MODE_FNC1SECOND
+MODE_VALUES = dict(
+    (name, val) for (name, val) in globals().items()
+    if name.startswith("MODE_")
+)
+MODE_NAMES = dict((v, k) for (k, v) in MODE_VALUES.items())
 
 ECLEVEL_L = lib.QR_ECLEVEL_L
 ECLEVEL_M = lib.QR_ECLEVEL_M
@@ -76,7 +93,9 @@ class QRCode(object):
             if err:
                 if ffi.errno == lib.ENOMEM:
                     raise MemoryError("Couldn't allocate memory for QR code")
-                raise ValueError("Invalid QR code mode (%r) or data" %(mode, ))
+                raise ValueError("Invalid mode (%s) + data (%r) combination" %(
+                    MODE_NAMES[mode], data,
+                ))
 
             qrc = lib.QRcode_encodeInput(input)
             if qrc == ffi.NULL:
@@ -84,18 +103,61 @@ class QRCode(object):
                     raise MemoryError("Couldn't allocate memory for QR code")
                 raise ValueError("Invalid QR code input")
             to_free.append((lib.QRcode_free, qrc))
-            self.width = qrc.width
-            self.raw_data = list(qrc.data[0:self.width * self.width])
+            self.size = qrc.width
+            self.raw_data = list(qrc.data[0:self.size * self.size])
         finally:
             for func, arg in to_free:
                 func(arg)
 
     def get_raw_data(self):
+        """ Returns the raw QR code data. Values must be masked with one of
+            the DATA_* constants to get something meaningful out of them.
+
+                >>> qr = QRCode("hello")
+                >>> qr.data[:5]
+
+            """
         return self.raw_data
 
     def get_im(self, border=None, module_size=None):
-        if PIL is None:
-            raise ImportError("PIL must be installed to use get_im")
+        """ Returns a PIL Image. """
+        if Image is None:
+            raise ImportError("PIL could not be imported")
         border = 4 if border is None else border
         module_size = 3 if module_size is None else module_size
-        bytes = []
+        im_bytes = []
+        for idx, b in enumerate(self.raw_data):
+            im_bytes.extend([(~b & DATA_BW) * 255] * module_size)
+            if (idx + 1) % self.size == 0:
+                im_bytes.extend(im_bytes[-(self.size * module_size):] * (module_size - 1))
+        im_size = (self.size * module_size, self.size * module_size)
+        im = Image.frombytes('L', im_size, bytes(bytearray(im_bytes)))
+        if border > 0:
+            im = ImageOps.expand(im, border * module_size, 255)
+        return im
+
+    def get_svg_string(self, encoding=None):
+        encoding = encoding or "us-ascii"
+        return ET.tostring(self.get_svg_etree(), encoding=encoding)
+
+    def get_svg_etree(self):
+        """ Returns an ElementTree SVG <g> element containing the QR code. """
+        if ET is None:
+            raise ImportError("lxml.etree or xml.elementtree could not be imported")
+        g = ET.Element("g")
+        g.append(ET.Element(
+            "rect", fill="white",
+            x="0", y="0",
+            width=str(self.size), height=str(self.size),
+        ))
+        for idx, b in enumerate(self.raw_data):
+            if ~b & DATA_BW:
+                continue
+            x = idx // self.size
+            y = idx % self.size
+            g.append(ET.Element(
+                "rect", fill="black",
+                x=str(x), y=str(y),
+                width="1", height="1",
+            ))
+        return g
